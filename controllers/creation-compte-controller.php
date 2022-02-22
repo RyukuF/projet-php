38f2693554970a6e56bf7f3e7ce891b9ea20b347
Charles MARQUES <marques.charles91@gmail.com>
2022-02-22 11:20:29 +0100

    ajout de la confirmation de mdp et de l'avatar à la création de compte

--- controllers/creation-compte-controller.php
+++ controllers/creation-compte-controller.php
@@ -5,8 +5,11 @@ if (!empty($_POST)) {
     if (
         !empty($_POST['identifiant'])
         && !empty($_POST['password'])
-
+        && !empty($_POST['confirm-password'])
+        && $_POST['password'] == $_POST['confirm-password']
         && !empty($_POST['pseudo'])
+        && !empty($_POST['avatar'])
+        && filter_var($_POST['avatar'], FILTER_VALIDATE_URL) !== false
 
     ) {
         require_once model('Utilisateur');
@@ -15,10 +18,13 @@ if (!empty($_POST)) {
         $utilisateur->identifiant = $_POST['identifiant'];
         $utilisateur->mot_de_passe = password_hash($_POST['password'], PASSWORD_BCRYPT);
         $utilisateur->pseudo = $_POST['pseudo'];
+        $utilisateur->avatar = $_POST['avatar'];
 
         $utilisateur->save();
 
         redirection('connexion');
+    } else if ($_POST['password'] != $_POST['confirm-password']){
+        $error_mdp = true;
     } else $error = true;
 }
 
--- views/creation-compte.php
+++ views/creation-compte.php
@@ -14,6 +14,15 @@
         </div>
     <?php } ?>
 
+    <?php if (!empty($error_mdp)) { ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <strong>Oups !</strong> Les mots de passe ne correspondent pas
+        </div>
+    <?php } ?>
+
     <div class="form-group row">
         <label for="identifiant" class="col-12 col-form-label">Identifiant :</label>
         <div class="col-12">
@@ -29,9 +38,23 @@
     </div>
 
     <div class="form-group row">
+        <label for="confirm-password" class="col-12 col-form-label">Confirmation mot de passe :</label>
+        <div class="col-12">
+            <input type="password" class="form-control" name="confirm-password" id="confirm-password" placeholder="Votre mot de passe" required>
+        </div>
+    </div>
+
+    <div class="form-group row">
         <label for="pseudo" class="col-12 col-form-label">Pseudo :</label>
         <div class="col-12">
             <input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="Votre pseudo" required>
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="avatar" class="col-12 col-form-label">Avatar (Un url vers une image hébergée est demandé) :</label>
+        <div class="col-12">
+            <input type="url" class="form-control" name="avatar" id="avatar" placeholder="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Wolf_at_Woburn_Safari_Park.jpg/1280px-Wolf_at_Woburn_Safari_Park.jpg" required>
         </div>
     </div>
 
